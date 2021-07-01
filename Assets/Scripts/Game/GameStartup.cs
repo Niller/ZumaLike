3f6c31666a966f50dc3d8f550a8f30e5ea2fd1bd
Aleksandr Borisov <nillerprog@gmail.com>
2021-07-01 23:21:49 +0300

    Add Leo.ECS

---
+++ Assets/Scripts/Game/GameStartup.cs
@@ -0,0 +1,38 @@
+﻿using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+        
+        void Start () 
+        {
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
+            _systems.Init();
+        }
+        
+        void Update () 
+        {
+            _systems?.Run ();
+        }
+
+        void OnDestroy () 
+        {
+            if (_systems != null) 
+            {
+                _systems.Destroy();
+                _systems = null;
+            }
+            
+            if (_world != null) 
+            {
+                _world.Destroy();
+                _world = null;
+            }
+        }
+    }
+}
